d3a86978982376c61e1657af7078ca70c533aaae
TruonggDX <dinhxuantruong2003coi@gmail.com>
2024-01-27 21:08:41 +0700

    show password, carousel

---
+++ Clothes_SpringBoot/src/main/java/edu/t3h/clothes/Config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package edu.t3h.clothes.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests(http -> http
+                        .requestMatchers("/login","/auth/**").permitAll()
+                        .anyRequest().authenticated()).logout(httpLogout -> httpLogout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login"))
+                .formLogin(httpLogin ->
+                        httpLogin.loginPage("/login")
+                                .usernameParameter("username")
+                                .permitAll()
+                                .successHandler((request, response, authentication) -> {
+//                                    for (GrantedAuthority authority : authentication.getAuthorities()) {
+////                                        if (authority.getAuthority().equals("USER")) {
+//                                            response.sendRedirect("/user");
+//                                            return;
+////                                        }
+//                                    }
+                                    response.sendRedirect("/user");
+                                })
+                )
+
+
+                .httpBasic()
+                .and()
+                .build();
+    }
+
+}
