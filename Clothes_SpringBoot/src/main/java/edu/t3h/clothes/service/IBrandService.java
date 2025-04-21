6900590ff27bf6c80321f2fff4ad3b129c391162
ZyZy2223 <chuduy20022003@gmail.com>
2025-04-21 17:48:43 +0700

    Brand

--- Clothes_SpringBoot/src/main/java/edu/t3h/clothes/service/IBrandService.java
+++ Clothes_SpringBoot/src/main/java/edu/t3h/clothes/service/IBrandService.java
@@ -1,13 +1,14 @@
 package edu.t3h.clothes.service;
 
-
 import edu.t3h.clothes.model.dto.BrandDto;
+import edu.t3h.clothes.model.response.ResponsePage;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface IBrandService {
   BrandDto createBrand(BrandDto brandDto);
   BrandDto getBrandById(Long id);
-  List<BrandDto> getAllBrands();
+  ResponsePage<List<BrandDto>> getBrands(Pageable pageable);
   BrandDto updateBrand(Long id, BrandDto brandDto);
   void deleteBrand(Long id);
 }
--- Clothes_SpringBoot/src/main/java/edu/t3h/clothes/service/impl/BrandServiceImpl.java
+++ Clothes_SpringBoot/src/main/java/edu/t3h/clothes/service/impl/BrandServiceImpl.java
@@ -3,13 +3,17 @@ package edu.t3h.clothes.service.impl;
 import edu.t3h.clothes.entity.BrandEntity;
 import edu.t3h.clothes.mapper.BrandMapper;
 import edu.t3h.clothes.model.dto.BrandDto;
+import edu.t3h.clothes.model.response.ResponsePage;
 import edu.t3h.clothes.repository.BrandRepository;
 import edu.t3h.clothes.service.IBrandService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -33,11 +37,19 @@ public class BrandServiceImpl implements IBrandService {
   }
 
   @Override
-  public List<BrandDto> getAllBrands() {
-    // Replace collect(Collectors.toList()) with Stream.toList() to get an unmodifiable list
-    return brandRepository.findAll().stream()
+  public ResponsePage<List<BrandDto>> getBrands(Pageable pageable) {
+    Page<BrandEntity> page = brandRepository.findAll(pageable);
+    List<BrandDto> dtos = page.stream()
         .map(brandMapper::toDto)
         .toList();
+
+    ResponsePage<List<BrandDto>> resp = new ResponsePage<>();
+    resp.setContent(dtos);
+    resp.setPageNumber(page.getNumber());
+    resp.setPageSize(page.getSize());
+    resp.setTotalElements(page.getTotalElements());
+    resp.setTotalPages(page.getTotalPages());
+    return resp;
   }
 
   @Override
