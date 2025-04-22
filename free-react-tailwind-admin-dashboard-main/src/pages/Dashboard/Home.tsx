a0142304d8c1569409a61cef69b337cda9295a45
TruonggDX <dinhxuantruong2003coi@gmail.com>
2025-04-22 22:41:16 +0700

    template fe for admin

---
+++ free-react-tailwind-admin-dashboard-main/src/pages/Dashboard/Home.tsx
@@ -0,0 +1,41 @@
+import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
+import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
+import StatisticsChart from "../../components/ecommerce/StatisticsChart";
+import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
+import RecentOrders from "../../components/ecommerce/RecentOrders";
+import DemographicCard from "../../components/ecommerce/DemographicCard";
+import PageMeta from "../../components/common/PageMeta";
+
+export default function Home() {
+  return (
+    <>
+      <PageMeta
+        title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
+        description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+      />
+      <div className="grid grid-cols-12 gap-4 md:gap-6">
+        <div className="col-span-12 space-y-6 xl:col-span-7">
+          <EcommerceMetrics />
+
+          <MonthlySalesChart />
+        </div>
+
+        <div className="col-span-12 xl:col-span-5">
+          <MonthlyTarget />
+        </div>
+
+        <div className="col-span-12">
+          <StatisticsChart />
+        </div>
+
+        <div className="col-span-12 xl:col-span-5">
+          <DemographicCard />
+        </div>
+
+        <div className="col-span-12 xl:col-span-7">
+          <RecentOrders />
+        </div>
+      </div>
+    </>
+  );
+}
